afbe44bfd49b5aecf2d65eef2dbb865a06151648
Courtney Barley <courtneybarley@IdahoiMac.local>
2017-03-21 18:38:08 -0700

    Assignment 9: Dom Scripting

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,35 @@
 var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
+    
     var revealPoint = function(index) {
-    for(var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
-            points[index].style.opacity = 1;
-            points[index].style.transform = "scaleX(1) translateY(0)";
-            points[index].style.msTransform = "scaleX(1) translateY(0)";
-           points[index].style.WebkitTransform = "scaleX(1) translateY(0)";  
-            
-            }
-
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";  
+    //     for(var i = 0; i < points.length; i++) {
+//        revealPoint(i);
+//     }
 
 };
 
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
+};
+
+var animatePoints = function(point) {
+
 window.onload = function () {
+    
     if (window.innerHeight > 950) {
+        alert("This should run");
         animatePoints(pointsArray);
     }
     var sellingPoints = document.getElementsByClassName('selling-points') [0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-window.addEventListener("scroll", function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);
-    }
-});
+    window.addEventListener("scroll", function(event) {
+        console.log("scrolled here");
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            console.log("should animate here");
+            animatePoints(pointsArray);
+        }
+    });
 }
