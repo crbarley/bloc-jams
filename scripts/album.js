cc3aeca7be0ba1531cdf4bcae4cdab0e924a5064
Courtney Barley <courtneybarley@IdahoiMac.local>
2017-03-30 15:06:03 -0700

    Assignment 11 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,6 @@
 var albumPicasso = {
     title: 'The Colors',
-    artist: 'Pabloc Picasso',
+    artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
@@ -25,8 +25,23 @@ var albumMarconi = {
         { title: 'Hello, Operator?', duration: '1:01' },
         { title: 'Ring, ring, ring', duration: '5:01' },
         { title: 'Fits in your pocket', duration: '3:21' },
-        { title: 'Can you hear me now?', duration: '2:15' }
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }
     
+    ]
+};
+
+var albumMesozoic = {
+    title: 'The Jurassic',
+    artist: 'Mesozoic Era',
+    label: 'Asteroid',
+    year: '135 M BC',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Stegosaurus', duration: '4:21' },
+        { title: 'Brachiosaurus', duration: '3:36' },
+        { title: 'Brantosaurus', duration: '5:16' },
+        { title: 'Megalosaurus', duration: '3:47' }
     ]
 };
 
@@ -42,13 +57,15 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-    
-    var albumTitle = document.getElementsByClassNameyClassName('album-view-title')[0];
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
     var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
+    
+
     
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -67,4 +84,16 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+    
+    var albums = [albumPicasso, albumMarconi, albumMesozoic];
+    var index = 1;
+    
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
 };
+
