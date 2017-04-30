60d85a7df5b5428cecf1b19c857d7d997579bbcf
Courtney Barley <crbarley@gmail.com>
2017-04-29 17:58:29 -0700

    Assignment 20: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -183,6 +183,21 @@ var updatePlayerBarSong = function() {
 };
 
 
+var togglePlayFromPlayerBar = function() {
+    
+    if(currentSoundFile.isPaused()) {
+        
+        $(this).html(pauseButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else {
+        
+        $(this).html(playButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
+
 
 var playButtonTemplate = '<a class ="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -197,12 +212,14 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $controls = $('.main-controls .play-pause');
+
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
-    
+    $controls.click(togglePlayFromPlayerBar);
 });
 
 
